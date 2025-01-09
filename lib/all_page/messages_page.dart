664b41f5d90e7fe98e3945bd2357de2dd0a1723d
bk1784 <futonabuu456@gmail.com>
2025-01-09 19:30:49 +0700

    membuat halaman pesan/messages

---
+++ lib/all_page/messages_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MessagesPage extends StatelessWidget {
+  const MessagesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Messages",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:template_project/all_page/list_page.dart';
 import 'package:template_project/all_page/profile_page.dart';
 import 'package:template_project/screen/home_screen.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ProfilePage(email: '',),
+      home: CryptoScreen(),
     );
   }
 }
